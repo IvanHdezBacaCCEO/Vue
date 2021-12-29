e2ce7a0e5d65b8ed17a30117b211b3174828d6a4
IvanHdezBacaCCEO <manuel.hernandez@cceo.com.mx>
2021-12-29 12:51:46 -0600

    finished the course's section7 project

---
+++ section7/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="container">
+  <div class="jumbotron">
+    <button class="btn btn-primary"
+      @click="selectedComponent='LogIn'">
+      Iniciar Sesion</button>
+    <button class="btn btn-primary"
+      @click="selectedComponent='ChangePassword'">
+      Cambiar Contrasena</button>
+    <keep-alive>
+      <component v-bind:is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</div>
+</template>
+
+<script>
+import ShowForm from './ShowForm.vue';
+import ChangePassword from './ChangePassword.vue';
+import LogIn from './LogIn.vue';
+
+export default {
+  components: {
+    ShowForm: ShowForm,
+    ChangePassword: ChangePassword,
+    LogIn: LogIn
+  },
+  data () {
+    return {
+      selectedComponent: 'LogIn'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.jumbotron {
+  padding: 2rem 1rem;
+  margin-bottom: 2rem;
+  background-color: #e9ecef;
+  border-radius: .3rem;
+}
+</style>
---
+++ section7/src/ShowForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <form action="" class="form">
+        <slot name="title"></slot>
+        <slot name="elements"></slot>
+        <slot name="button"></slot>
+    </form>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+h3 {
+  color: green
+}
+</style>
