ba472713369cefb17bbb2c054ca65f3207800a8f
IvanHdezBacaCCEO <manuel.hernandez@cceo.com.mx>
2021-12-29 15:28:35 -0600

    finished the course's section10 project

---
+++ section10/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <span>
+        {{message | caps}}
+        <hr>
+        {{message | suspensive}}
+        <hr>
+        {{message | caps | suspensive}}
+        <hr>
+        {{message}}
+        <hr>
+        Celsius: {{temperature}} <br>
+        Farenheit: {{temperature | toFarenheit}}
+
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      message: 'Aprende Vue.js 2 Facilmente',
+      temperature: 20
+    }
+  },
+  filters : {
+    caps(message) {
+      return message.toUpperCase();
+    },
+    toFarenheit(temp) {
+      return (9/5 * temp) + 32 + 'F'
+    }
+  }
+}
+</script>
+
+<style>
+.jumbotron {
+  padding: 2rem 1rem;
+  margin-bottom: 2rem;
+  background-color: #e9ecef;
+  border-radius: .3rem;
+}
+</style>
